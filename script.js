6448335836e2069a42e4596e0492ddb8df0f844f
Oscar <palomecoscar@gmail.com>
2021-04-07 23:57:39 -0500

    Agregué funciones para hacer funcionar la calculadora

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const display = document.querySelector("#calculator-display");
 const buttonNumbers = document.querySelectorAll(".number");
 const equalbutton = document.querySelector("#equal-button");
 const operators = document.querySelectorAll(".operator");
+const clearbutton = document.querySelector("#clear-button");
 let number1 ="";
 let number2 ="";
+let operation = "";
 
 let add = ((n1,n2) => n1+n2);
 
@@ -13,7 +15,25 @@ let multiply = ((n1,n2) => n1*n2);
 
 let divide = ((n1,n2) => n1/n2);
 
-let operate = ((operator, n1,n2) => console.log(operator));
+let operate = ((operator, n1,n2) => {
+    console.log(operator, n1, n2 );
+    switch (operator){
+        case "+":
+            return add(n1, n2);
+            break;
+        case "-":
+            return substract(n1, n2);
+            break;
+        case "X":
+            return multiply(n1,n2);
+            break;
+        case "/":
+            return divide(n1,n2);
+            break;
+        default:
+            console.log("ERROR");
+    }
+});
 
 let populateDisplay = (() => {
     console.log("display jeje");
@@ -27,4 +47,25 @@ buttonNumbers.forEach((button) => {
     });
 });
 
-equalbutton.addEventListener("click", () => console.log(number1));
+equalbutton.addEventListener("click", () => {
+    console.log(number1);
+    display.textContent = operate(operation, parseInt(number2), parseInt(number1));
+    number2 = "";
+    number1 = "";
+});
+
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        display.textContent = "";
+        number2 = number1;
+        operation = operator.textContent;
+        console.log(operation);
+    });
+});
+
+clearbutton.addEventListener("click", () => {
+    display.textContent = "";
+    number2 = "";
+    number1 = "";
+});
