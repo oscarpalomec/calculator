6be31c5d7e0e0b60a7072721e4b7d6aefb3892f3
Oscar <palomecoscar@gmail.com>
2021-04-10 20:14:14 -0500

    Se pueden encadenar operaciones

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const buttonNumbers = document.querySelectorAll(".number");
 const equalbutton = document.querySelector("#equal-button");
 const operators = document.querySelectorAll(".operator");
 const clearbutton = document.querySelector("#clear-button");
-let number1 ="";
-let number2 ="";
+let number1 =0;
+let number2 =0;
 let operation = "";
+let operation2 = "";
 
 let add = ((n1,n2) => n1+n2);
 
@@ -13,7 +14,14 @@ let substract = ((n1,n2) => n1-n2);
 
 let multiply = ((n1,n2) => n1*n2);
 
-let divide = ((n1,n2) => n1/n2);
+let divide = ((n1,n2) => {
+    if (n2 == 0){
+        return "really?";
+    }else{
+        return n1/n2;
+    }
+    
+});
 
 let operate = ((operator, n1,n2) => {
     console.log(operator, n1, n2 );
@@ -42,30 +50,54 @@ let populateDisplay = (() => {
 
 buttonNumbers.forEach((button) => {
     button.addEventListener("click", () => {
-        display.textContent = display.textContent + button.textContent;
+        if (number2==0){
+            display.textContent = display.textContent + button.textContent;
+        }else {
+            display.textContent =  button.textContent;
+        }
+       
         number1 = display.textContent;
     });
 });
 
 equalbutton.addEventListener("click", () => {
     console.log(number1);
-    display.textContent = operate(operation, parseInt(number2), parseInt(number1));
-    number2 = "";
-    number1 = "";
+    display.textContent = operate(operation2, parseInt(number2), parseInt(number1));
+    number2 = display.textContent;
+    number1 = 0;
 });
 
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
-        display.textContent = "";
-        number2 = number1;
         operation = operator.textContent;
-        console.log(operation);
+        display.textContent = "";
+        if (number2 != 0 & number1 !=0){
+            console.log("Entróoo")
+            operation2=operation
+            display.textContent = operate(operation2, parseInt(number1), parseInt(number2));
+            number2 = display.textContent;
+            number1=0;
+            
+        }else if (number2 != 0 & number1==0){
+            console.log("Entró")
+            operation2=operation
+            //display.textContent = operate(operation2, parseInt(number1), parseInt(number2));
+            //number2 = display.textContent;
+
+        }else {
+          
+            number2 = number1;
+            operation2 = operation;
+        }
+       
+       
+        
     });
 });
 
 clearbutton.addEventListener("click", () => {
     display.textContent = "";
-    number2 = "";
-    number1 = "";
+    number2 = 0;
+    number1 = 0;
 });
